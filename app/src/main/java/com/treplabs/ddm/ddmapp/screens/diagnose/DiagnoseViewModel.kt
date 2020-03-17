29d1c6f18522228eae32235cb11072d1990e980d
Rasheed Sulayman <sulaymanrasheed@gmail.com>
2020-03-17 20:17:18 +0100

    Implement diagnose flow landing page.

--- app/src/main/java/com/treplabs/ddm/ddmapp/screens/diagnose/DiagnoseViewModel.kt
+++ app/src/main/java/com/treplabs/ddm/ddmapp/screens/diagnose/DiagnoseViewModel.kt
@@ -1,9 +1,19 @@
 package com.treplabs.ddm.ddmapp.screens.diagnose
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.treplabs.ddm.base.BaseViewModel
+import com.treplabs.ddm.utils.Event
 import javax.inject.Inject
 
 class DiagnoseViewModel @Inject constructor() : BaseViewModel() {
+
+    private val _user = MutableLiveData<FirebaseUser>(FirebaseAuth.getInstance().currentUser)
+
+    val user: LiveData<FirebaseUser>
+        get() = _user
 
     override fun addAllLiveDataToObservablesList() {
     }
